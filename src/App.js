08f2be3598eb433c3e743288bb0dae2559b483cd
sunghyunjo <tjdgusdl2293@gmail.com>
2019-09-04 22:28:22 +0900

    #2 scroll zoom하는 중

--- src/App.js
+++ src/App.js
@@ -1,60 +1,82 @@
 import React, { Component } from "react";
 import * as THREE from "three";
+import { CompressedPixelFormat } from "three";
 
 class App extends Component {
-  componentDidMount() {
-    // === THREE.JS CODE START ===
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
 
-    // ************** no1 
-    var geom = new THREE.Geometry();
-    var v1 = new THREE.Vector3(0,0,0);
-    var v2 = new THREE.Vector3(15,0,0);
-    var v3 = new THREE.Vector3(15,15,0);
-    
-    geom.vertices.push(v1);
-    geom.vertices.push(v2);
-    geom.vertices.push(v3);
-    
-    geom.faces.push( new THREE.Face3( 0, 1, 2 ) );
-    // ******************
-
-    ///// *********** no2
-    var triangle = new THREE.Triangle( v1, v2, v3 );
-    var normal = triangle.normal();
-
-    // An example of getting the area from the Triangle class
-    console.log( 'Area of triangle is: '+ triangle.area() );
-
-    geom.vertices.push(triangle.a);
-    geom.vertices.push(triangle.b);
-    geom.vertices.push(triangle.c);
-
-    geom.faces.push( new THREE.Face3( 0, 1, 2, normal ) );
-    ////// *********
-
-    geom.computeFaceNormals();
-    
-    var cube= new THREE.Mesh( geom, new THREE.MeshNormalMaterial() );
-
-    scene.add( cube );
-    camera.position.z = 10;
-    var animate = function () {
-      requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
-    };
-    animate();
+  componentDidMount(){
+    const width = this.mount.clientWidth;
+    const height = this.mount.clientHeight;
+    //ADD SCENE
+    this.scene = new THREE.Scene();
+    //ADD CAMERA
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      width / height,
+      0.1,
+      1000
+    );
+    this.camera.position.z = 100;
+    //ADD RENDERER
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setClearColor('#000000');
+    this.renderer.setSize(width, height);
+    this.mount.appendChild(this.renderer.domElement);
+    //ADD CUBE
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: '#433F81'});
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+    this.start();
+    window.addEventListener('scroll', this.onScroll);
   }
 
-  render() {
-    return (
-      <div ref={ref => (this.mount = ref)} />
+componentWillUnmount(){
+    this.stop();
+    this.mount.removeChild(this.renderer.domElement);
+}
+
+start = () => {
+    if (!this.frameId) {
+      this.frameId = requestAnimationFrame(this.animate);
+    }
+}
+
+stop = () => {
+    cancelAnimationFrame(this.frameId);
+}
+
+animate = () => {
+   this.cube.rotation.x += 0.01;
+   this.cube.rotation.y += 0.01;
+   this.renderScene();
+   this.frameId = window.requestAnimationFrame(this.animate);
+}
+
+renderScene = () => {
+  this.renderer.render(this.scene, this.camera)
+}
+
+onScroll = () => {
+  const scroll = window.scrollY;
+
+  if(scroll > 0) {
+    this.camera.position.z -= 3;
+  } else {
+    this.camera.position.z += 3;
+  }
+
+  console.log(window.scrollY);
+  console.log(this.camera.position.z);
+  
+}
+
+render(){
+    return(
+      <div
+        style={{ width: '100%', height: '100vh' }}
+        ref={(mount) => { this.mount = mount }}
+      />
     )
   }
 }
